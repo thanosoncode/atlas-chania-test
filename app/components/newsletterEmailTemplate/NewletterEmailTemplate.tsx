c40f230cb05d883f679dd84f92685a98ef1411d4
thanosoncode <74588018+thanosoncode@users.noreply.github.com>
2023-10-12 12:35:30 +0300

    template appearance test

--- app/components/newsletterEmailTemplate/NewletterEmailTemplate.tsx
+++ app/components/newsletterEmailTemplate/NewletterEmailTemplate.tsx
@@ -11,19 +11,24 @@ interface NewsletterEmailTemplateProps {
 const NewsletterEmailTemplate: React.FC<
   Readonly<NewsletterEmailTemplateProps>
 > = ({ blog }) => (
-  <div className="flex flex-col  gap-6 items-center justify-center">
-    <h1 style={{ fontSize: "22px", fontWeight: "normal" }}>
-      Atlas Chania newsletter!
-    </h1>
-    <p style={{ fontWeight: "bold" }}>{blog.title}</p>
-    <img
-      src={blog.image}
-      alt={blog.title}
-      style={{ width: "200px", height: "120px" }}
-    />
-    <a href={`https://www.atlaschania.gr/posts/${blog.id}`}>
-      {`https://www.atlaschania.gr/posts/${blog.id}`}
-    </a>
+  <div>
+    <div className="flex flex-col  gap-6 items-center justify-center">
+      <h1 style={{ fontSize: "22px", fontWeight: "normal" }}>
+        Atlas Chania newsletter!
+      </h1>
+      <p style={{ fontWeight: "bold", fontSize: "20px" }}>{blog.title}</p>
+      <img
+        src={blog.image}
+        alt={blog.title}
+        style={{ width: "340px", height: "280px", display: "block" }}
+      />
+      <a
+        href={`https://www.atlaschania.gr/posts/${blog.id}`}
+        style={{ display: "block" }}
+      >
+        {`https://www.atlaschania.gr/posts/${blog.id}`}
+      </a>
+    </div>
   </div>
 );
 
