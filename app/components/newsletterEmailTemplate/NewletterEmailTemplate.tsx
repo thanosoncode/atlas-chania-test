f6f2ad4f44ca8de8fd7d34a188d7148a50608210
thanosoncode <74588018+thanosoncode@users.noreply.github.com>
2023-10-12 12:28:40 +0300

    testing newsletter template

--- app/components/newsletterEmailTemplate/NewletterEmailTemplate.tsx
+++ app/components/newsletterEmailTemplate/NewletterEmailTemplate.tsx
@@ -12,9 +12,15 @@ const NewsletterEmailTemplate: React.FC<
   Readonly<NewsletterEmailTemplateProps>
 > = ({ blog }) => (
   <div className="flex flex-col  gap-6 items-center justify-center">
-    <h1 className="text-xl font-normal">Atlas Chania newsletter!</h1>
-    <p className="font-bold">{blog.title}</p>
-    <img src={blog.image} alt={blog.title} className="w-40 h-40" />
+    <h1 style={{ fontSize: "22px", fontWeight: "normal" }}>
+      Atlas Chania newsletter!
+    </h1>
+    <p style={{ fontWeight: "bold" }}>{blog.title}</p>
+    <img
+      src={blog.image}
+      alt={blog.title}
+      style={{ width: "200px", height: "120px" }}
+    />
     <a href={`https://www.atlaschania.gr/posts/${blog.id}`}>
       {`https://www.atlaschania.gr/posts/${blog.id}`}
     </a>
